1e2a8ca58eec36d99f739c498e13fa891119a6f8
Nkanyiso321 <nkanyisodube1234@gmail.com>
2025-10-12 13:42:00 +0200

    add  class_static_methods_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,21 +1,13 @@
-# polymorphism_demo.py
-import math
+# class_static_methods_demo.py
 
-class Shape:
-    def area(self):
-        raise NotImplementedError("Subclasses must override the area method")
+class Calculator:
+    calculation_type = "Arithmetic Operations"
 
-class Rectangle(Shape):
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-    
-    def area(self):
-        return self.length * self.width
+    @staticmethod
+    def add(a, b):
+        return a + b
 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-    
-    def area(self):
-        return math.pi * self.radius ** 2
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
